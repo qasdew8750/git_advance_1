3481e95219fc6b1ca28484674a3fd5c558875b51
qasdew8750 <car58338@naver.com>
2024-04-18 21:19:11 +0900

    sum_of_squares_of_even commit

--- main.py
+++ main.py
@@ -21,7 +21,11 @@ Open-Source Software Practice 3
  The sum of the squares of all even numbers in the list.
  """
  # TODO: Implement sum_of_squares_of_even
- pass
+    output = 0
+    for i in even_int_list:
+        if(i % 2 == 0)
+            output = output + i
+    return output
 # Main function
 def main():
  # Example list
